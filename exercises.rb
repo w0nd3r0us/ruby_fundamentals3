99a6b35415b605e6782a09b8badd1ca2a339b1b3
w0nd3r0us <steven.grewal@gmail.com>
2018-05-24 17:56:45 -0400

    ex4 completed

--- exercises.rb
+++ exercises.rb
@@ -7,30 +7,30 @@ artists = ["Diplo", "Faux", "Celin Dion"]
 fav_colours2 = [:Blue, :Green, :Purple, :Magenta, :Gold]
 
 words_hash = {
-  :program => "A planned series of future events, items, or performances.",
-  :tree => "A woody perennial plant.",
-  :drive => "Operate and control the direction and speed of a motor vehicle."
+  "Program" => "A planned series of future events, items, or performances.",
+  "Tree" => "A woody perennial plant.",
+  "Drive" => "Operate and control the direction and speed of a motor vehicle."
 }
 
 movies_hash = {
-  :aliens => 1986,
-  :dieHard => 1988,
-  :ferrisBuellersDayOff => 1986,
-  :johnWick => 2014
+  "Aliens" => 1986,
+  "Die Hard" => 1988,
+  "Ferris Bueller's Day Off" => 1986,
+  "John Wick" => 2014
 }
 
 cities_hash = {
-  :tokyo => 13740000,
-  :paris => 2244000,
-  :london => 8788000
+  "Tokyo" => 13740000,
+  "Paris" => 2244000,
+  "London" => 8788000
 }
 
-names_has = {
-  :leon => 31,
-  :geeta => 31,
-  :tony => 33,
-  :jeff => 35,
-  :rajan => 27
+names_hash = {
+  "Leon" => 31,
+  "Geeta" => 31,
+  "Tony" => 33,
+  "Jeff" => 35,
+  "Rajan" => 27
 }
 
 #exercise 1
@@ -43,18 +43,42 @@ puts ages.sort
 #4
 puts ages.push(0)
 #5
-puts movies_hash[:aliens]
+puts movies_hash["Aliens"]
 
 #exercise 2
 #1
 puts fav_colours[-1]
 #2
-cities_hash[:Moscow] = 12800000
+cities_hash["Moscow"] = 12800000
 #3
 coin_toss = coin_toss.reverse
 #4
-puts cities_hash[:Moscow]
+puts cities_hash["Moscow"]
 #5
 artists.each do |v|
   puts "I think #{v} is great"
 end
+
+#exercise 3
+#1
+puts artists[0..1]
+#2
+movies_hash.each do |n, y|
+  puts "#{n} came out in #{y}."
+end
+#3
+puts ages = ages.sort.reverse #ages.reverse!
+#4
+puts movies_hash["Beauty And The Beast"] = [1991, 2017]
+
+#exercise 4
+#1
+puts ages.select {|age| age <= 32}
+#2
+puts ages.max
+#3
+puts coin_toss.count("yes")
+#4
+artists.delete_at(2)
+#5
+cities_hash["Moscow"] = 13500000
