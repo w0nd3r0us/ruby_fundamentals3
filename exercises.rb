2ef763fe0542ad5f448478f56b669560e6df258b
w0nd3r0us <steven.grewal@gmail.com>
2018-05-24 16:36:05 -0400

    commit with correct file and ex0 completed

---
+++ exercises.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+#exercise 0
+fav_colours = ["Blue", "Green", "Purple", "Magenta", "Gold"]
+ages = [31 ,31, 33, 35, 27]
+coin_toss = ["yes", "no", "yes", "yes", "no"]
+artists = ["Diplo", "Faux", "Celin Dion"]
+fav_colours2 = [:Blue, :Green, :Purple, :Magenta, :Gold]
+
+words_hash = {
+  :program => "A planned series of future events, items, or performances.",
+  :tree => "A woody perennial plant.",
+  :drive => "Operate and control the direction and speed of a motor vehicle."
+}
+
+movies_hash = {
+  :aliens => 1986,
+  :dieHard => 1988,
+  :ferrisBuellersDayOff => 1986,
+  :johnWick => 2014
+}
+
+cities_hash = {
+  :tokyo => 13740000,
+  :paris => 2244000,
+  :london => 8788000
+}
+
+names_has = {
+  :leon => 31,
+  :geeta => 31,
+  :tony => 33,
+  :jeff => 35,
+  :rajan => 27
+}
